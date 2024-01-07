8f429631437a2ab0fe0a6d0009f87cef5725ba3c
Anushka Kishor <kishoranushka96@gmail.com>
2024-01-07 16:07:04 +0530

    added code

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+# num=int(input("enter a number: "))
+# if num%2==0:
+#   print("even")
+# else:
+#   print("odd")
+
+
+num=int(input("enter a number: "))
+print("even") if num%2==0 else print("odd")
