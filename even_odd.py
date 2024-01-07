9ce83d21478f92b2faf640d7e914d19cc8a4dfb9
Anushka Kishor <kishoranushka96@gmail.com>
2024-01-07 16:08:15 +0530

    code

--- even_odd.py
+++ even_odd.py
@@ -1,9 +1,12 @@
+# Method 1
+# ----------
 # num=int(input("enter a number: "))
 # if num%2==0:
 #   print("even")
 # else:
 #   print("odd")
 
-
+# Method 2
+# -----------
 num=int(input("enter a number: "))
 print("even") if num%2==0 else print("odd")
