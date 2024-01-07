f8b448a13a3274e3c82f489729e7631449ad2f95
Anushka Kishor <kishoranushka96@gmail.com>
2024-01-07 16:03:35 +0530

    First

---
+++ vote.py
@@ -0,0 +1,21 @@
+# Method 1
+# -----------
+# age=int(input("enter your age: "))
+# if age>=18:
+#   print("Vote")
+# else:
+#   print("can not vote")
+
+
+# Method 2
+# -----------
+# age=int(input("enter your age: "))
+
+# print("Vote") if age>=18 else print("can not vote")
+
+# Method 3
+# -----------
+age=int(input("enter your age: "))
+result="Vote" if age>=18 else "can not vote"
+print(result)
+
